3842c3415a51d59dc75e41b00ee00a7794901393
SamuelVersiane <samuellversiane@gmail.com>
2022-01-11 15:32:32 -0300

    Entity Offer

---
+++ src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
@@ -0,0 +1,86 @@
+package com.devsuperior.dslearnbds.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_offer")
+public class Offer implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String Edition;
+    private Instant startMoment;
+    private Instant endMoment;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id") // PK
+    private Course course;
+
+    public Offer() {
+    }
+
+    public Offer(Long id, String edition, Instant startMoment, Instant endMoment, Course course) {
+        this.id = id;
+        Edition = edition;
+        this.startMoment = startMoment;
+        this.endMoment = endMoment;
+        this.course = course;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEdition() {
+        return Edition;
+    }
+
+    public void setEdition(String edition) {
+        Edition = edition;
+    }
+
+    public Instant getStartMoment() {
+        return startMoment;
+    }
+
+    public void setStartMoment(Instant startMoment) {
+        this.startMoment = startMoment;
+    }
+
+    public Instant getEndMoment() {
+        return endMoment;
+    }
+
+    public void setEndMoment(Instant endMoment) {
+        this.endMoment = endMoment;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Offer offer = (Offer) o;
+        return Objects.equals(id, offer.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
