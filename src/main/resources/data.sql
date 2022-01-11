b027157276c7201af94ab74b307736905517bebb
SamuelVersiane <samuellversiane@gmail.com>
2022-01-11 19:57:50 -0300

    Entity Resource

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,3 +16,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'aaaaa', 'aaaaa');
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);
+INSERT INTO tb_resource(title, description, position ,img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'aaaaaaaa', 1, 1);
+INSERT INTO tb_resource(title, description, position ,img_Uri, type, offer_id) VALUES ('Forum', 'Tire suas dúvidas', 2, 'aaaaaaaa', 2, 1);
+INSERT INTO tb_resource(title, description, position ,img_Uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas para a turma', 3, 'aaaaaaaa', 0, 1);
