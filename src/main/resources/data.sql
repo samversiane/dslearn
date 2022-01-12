9d0a389012eb22f568cd3a332e33cb3f5109da34
SamuelVersiane <samuellversiane@gmail.com>
2022-01-12 10:53:42 -0300

    Enrollment seed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,13 +14,17 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'aaaaa', 'aaaaa');
+
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);
 
-INSERT INTO tb_resource(title, description, position ,img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'aaaaaaaa', 1, 1);
-INSERT INTO tb_resource(title, description, position ,img_Uri, type, offer_id) VALUES ('Forum', 'Tire suas dúvidas', 2, 'aaaaaaaa', 2, 1);
-INSERT INTO tb_resource(title, description, position ,img_Uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas para a turma', 3, 'aaaaaaaa', 0, 1);
+INSERT INTO tb_resource (title, description, position ,img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'aaaaaaaa', 1, 1);
+INSERT INTO tb_resource (title, description, position ,img_Uri, type, offer_id) VALUES ('Forum', 'Tire suas dúvidas', 2, 'aaaaaaaa', 2, 1);
+INSERT INTO tb_resource (title, description, position ,img_Uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas para a turma', 3, 'aaaaaaaa', 0, 1);
 
-INSERT INTO tb_section(title, description, position ,img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 1', 'Nesete capítulo vamos começar', 1, 'aaaaaaaa', 1, null);
-INSERT INTO tb_section(title, description, position ,img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 2', 'Nesete capítulo vamos continuar', 2, 'aaaaaaaa', 1, 1);
-INSERT INTO tb_section(title, description, position ,img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 3', 'Nesete capítulo vamos finalizar', 3, 'aaaaaaaa', 1, 2);
+INSERT INTO tb_section (title, description, position ,img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 1', 'Nesete capítulo vamos começar', 1, 'aaaaaaaa', 1, null);
+INSERT INTO tb_section (title, description, position ,img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 2', 'Nesete capítulo vamos continuar', 2, 'aaaaaaaa', 1, 1);
+INSERT INTO tb_section (title, description, position ,img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 3', 'Nesete capítulo vamos finalizar', 3, 'aaaaaaaa', 1, 2);
+
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2020-11-20T13:00:00Z', null, true, false);
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2020-11-20T13:00:00Z', null, true, false);
