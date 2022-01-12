3c99e45ae2667d18a686461c8e967514365e3ff9
SamuelVersiane <samuellversiane@gmail.com>
2022-01-12 11:49:08 -0300

    Lesson, Content, Task

--- src/main/java/com/devsuperior/dslearnbds/entities/Section.java
+++ src/main/java/com/devsuperior/dslearnbds/entities/Section.java
@@ -2,6 +2,8 @@ package com.devsuperior.dslearnbds.entities;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -23,6 +25,9 @@ public class Section implements Serializable {
     @ManyToOne
     @JoinColumn(name = "prerequisite_id")
     private Section prerequisite;
+
+    @OneToMany(mappedBy = "section")
+    private List<Lesson> lessons = new ArrayList<>();
 
     public Section() {
     }
@@ -91,6 +96,10 @@ public class Section implements Serializable {
 
     public void setPrerequisite(Section prerequisite) {
         this.prerequisite = prerequisite;
+    }
+
+    public List<Lesson> getLessons() {
+        return lessons;
     }
 
     @Override
